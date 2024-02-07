dd4b2d0cc0cb0a193ffeb305b095586d4ed7d2f4
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-02-07 15:00:39 +0100

    basic express

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('test');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Home page');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About page</h1>');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('Not found');
+});
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000');
+});
