69c2f591e60fcc41c62dcd3f5c63248cc949891d
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-02-07 15:57:20 +0100

    send static file

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
+const PORT = 5000;
+
+//built-in middleware for serving static files
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.send('Home page');
-});
-
-app.get('/about', (req, res) => {
-  res.send('<h1>About page</h1>');
+  /* OR
+  const path = require('path');
+  res.sendFile(path.join(__dirname, '/src/index.html'))
+  res.sendFile(path.resolve('src/index.html'));
+*/
+  res.sendFile('./public/index.html', { root: __dirname });
 });
 
 app.all('*', (req, res) => {
   res.status(404).send('Not found');
 });
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
