8ce4d1702c35c41b3cf15c990d4fbb150569b532
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-02-08 18:01:16 +0100

    change code for api

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-//built-in middleware for serving static files
-//it serves index.html automatically in GET /, to remove this behavior use { index: false }
-app.use(express.static('./public'));
+app.get('/api', (req, res) => {
+  res.json([{ name: 'Rebeca' }, { name: 'Lucas' }]);
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('Not found');
+//search params / query strings
+app.get('/api/v1/query', (req, res) => {
+  console.log(req.query);
+  const queries = JSON.stringify(req.query);
+  res.send(`queries ${queries}`);
+});
+
+//url params
+app.get('/api/v1/:id', (req, res) => {
+  console.log(req.params.id);
+  const { id } = req.params;
+  res.send(`hello ${id}`);
 });
 
 app.listen(PORT, () => {
