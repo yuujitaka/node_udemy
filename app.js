41483e0550e8d7ade43f5b5069342493d39caaa3
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-02-09 18:38:31 +0100

    adding put and delete

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ app.post('/api/people', (req, res) => {
   res.status(201).json({ data: user });
 });
 
+app.put('/api/people/:id', (req, res) => {
+  const { user } = req.body;
+  const { id } = req.params;
+  if (!user || !id)
+    return res.status(400).json({ message: 'User and id required.' });
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) return res.status(404).json({ message: 'User not found.' });
+
+  res.status(201).json({ data: user });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(400).json({ message: 'ID required.' });
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) return res.status(404).json({ message: 'User not found.' });
+  res.status(201).json({ message: 'User deleted' });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
