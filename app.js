2b86842a140cdddb2366dde62820ed1a0125affc
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-02-13 14:25:56 +0100

    clearing the code

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express();
-const peopleRouter = require('./routes/people');
-const authRouter = require('./routes/auth');
 const PORT = 5000;
 
-app.use(express.static('./public'));
-//parses incoming requests with JSON payloads ex: req.body.property
-app.use(express.json());
-// to form-data
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/api/people', peopleRouter);
-app.use('/auth', authRouter);
+app.all('/', (req, res) => {
+  res.send('text');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
--- middleware/logger.js
+++
@@ -1,9 +0,0 @@
-// req => middleware => res
-const logger = (req, res, next) => {
-  const { method, url } = req;
-  const time = new Date();
-  console.log(method, url, time);
-  next();
-};
-
-module.exports = logger;
