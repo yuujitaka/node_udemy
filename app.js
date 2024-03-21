289ac74d4c0b3422813ceef632856001207dbaf1
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-03-21 19:18:45 +0100

    get,delete and update task and 404 route

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const routerTasks = require('./routes/tasks');
 const connectDB = require('./config/db');
+const notFound = require('./middleware/not-found');
 const PORT = 3000;
 
 connectDB();
 // to access req.body.property
 app.use(express.json());
 app.use('/api/v1/tasks', routerTasks);
+app.use(notFound);
 
 mongoose.connection.once('open', () => {
   console.log('Connected to mongoDB');
--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -18,16 +18,57 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.send('a task');
+const getTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findOne({ _id: taskId });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `No task with id ${taskId} was found` });
+    }
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send('update a task');
+const updateTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const newTask = req.body;
+
+    //overwrite: true if working with PUT
+    const task = await Task.findOneAndUpdate({ _id: taskId }, newTask, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `No task with id ${taskId} was found` });
+    }
+
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete a task');
+const deleteTask = async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const task = await Task.findOneAndDelete({ _id: taskId });
+    if (!task) {
+      return res
+        .status(404)
+        .json({ message: `No task with id ${taskId} was found` });
+    }
+    res.sendStatus(204);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
