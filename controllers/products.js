425edcb7d8ef3a1684e28a900541dec208877f91
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-04-02 15:20:34 +0200

    add sort, pagination and filtering

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,12 @@
 const Product = require('../model/product');
 
 const getAllProducts = async (req, res) => {
+  const { sort, fields, page, limit } = req.query;
+  const pageN = Number(page) || 1;
+  const limitN = Number(limit) || 10;
+  const skip = (pageN - 1) * limitN;
+
+  //add the property only if its value is defined
   const queryObject = (({ featured, company, name }) => {
     return {
       ...(featured && { featured: featured === 'true' ? true : false }),
@@ -9,7 +15,27 @@ const getAllProducts = async (req, res) => {
     };
   })(req.query);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  //sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  //send only the fields requested plus id
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+
+  //pagination
+  retult = result.skip(skip).limit(limit);
+
+  const products = await result;
+
   res.status(200).json(products);
 };
 
