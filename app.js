08fe28146f55dac4866cbdbf95c6e141153a85d5
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-04-05 13:24:51 +0200

    start project

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const express = require('express');
 require('express-async-errors');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const connectDB = require('./config/db');
-const productsRouter = require('./routes/products');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use('/api/v1/products/', productsRouter);
+app.use(express.static('./public'));
+app.use(express.json());
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const start = async () => {
-  await connectDB();
   app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 };
 
