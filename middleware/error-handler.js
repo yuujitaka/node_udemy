2df82578c5202563ec5b5b333f0d9744a2491a4f
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-04-12 20:48:32 +0200

    add custom mongoose errors and change regex

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,24 @@
 const { StatusCodes, ReasonPhrases } = require('http-status-codes');
 
+// eslint-disable-next-line no-unused-vars
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(err.status ?? StatusCodes.INTERNAL_SERVER_ERROR).json({
-    msg: err.message ?? `${ReasonPhrases.INTERNAL_SERVER_ERROR}: ${err}`,
+  let customError = {
+    statusCode: err.statusCode ?? StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message ?? `${ReasonPhrases.INTERNAL_SERVER_ERROR}: ${err}`,
+  };
+
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+        customError.statusCode = 409;
+        customError.message = `${Object.keys(err.keyValue.t)} already exist${
+          Object.keys.length > 1 ? '' : 's'
+        }`;
+    }
+  }
+
+  return res.status(customError.statusCode).json({
+    msg: customError.message,
   });
 };
 
