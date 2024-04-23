2aa3a59aac3269605bb7fd856a2f5430e3e9dbf3
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-04-23 18:14:50 +0200

    swagger

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
+//swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
 //extra secutity packages
 const helmet = require('helmet');
 const cors = require('cors');
@@ -16,6 +19,9 @@ const rateLimiter = require('express-rate-limit');
 const PORT = process.env.PORT || 3000;
 const app = express();
 connectDB();
+
+//swagger load
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.set('trust proxy', 1);
 app.use(
@@ -29,6 +35,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation </a>");
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticationMiddleware, jobsRouter);
 
