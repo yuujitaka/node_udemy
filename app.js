69de3d2900b2475db818a23278909dd586abbacb
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-04-23 17:10:34 +0200

    add security packages

--- app.js
+++ app.js
@@ -7,12 +7,27 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
+//extra secutity packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 connectDB();
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticationMiddleware, jobsRouter);
