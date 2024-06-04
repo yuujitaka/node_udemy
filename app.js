c4972b403e80f6eba7ca3a469dd181e954a9e14b
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-06-04 15:46:17 +0200

    add client folder and cleaning code

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('express-async-errors');
 const mongoose = require('mongoose');
 const connectDB = require('./config/db');
@@ -12,9 +13,7 @@ const swaggerUI = require('swagger-ui-express');
 const YAML = require('yamljs');
 //extra secutity packages
 const helmet = require('helmet');
-const cors = require('cors');
 const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -22,23 +21,13 @@ connectDB();
 
 //swagger load
 const swaggerDocument = YAML.load('./swagger.yaml');
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-  })
-);
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 
-app.get('/', (req, res) => {
-  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation </a>");
-});
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticationMiddleware, jobsRouter);
 
