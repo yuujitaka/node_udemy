1e4f2a561d49885a37edaa6151db8b4d1fb69f49
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-06-20 16:06:10 +0200

    clean up

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require('express');
 require('express-async-errors');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const sendEmail = require('./controllers/sendEmailController');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('home <a href="/send">send email</a>');
+  res.send('home');
 });
-
-app.get('/send', sendEmail);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
