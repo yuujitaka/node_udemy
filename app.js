178d3c18a02075552a4ef31102fb7d53aba829d6
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-06-20 16:18:50 +0200

    start project

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 require('express-async-errors');
+const path = require('path');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('home');
-});
-
+app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.json());
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
