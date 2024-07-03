20f158e09ee55c0e5a09b18004d9013883d5f625
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-07-03 14:11:10 +0200

    login and logout

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,7 +3,34 @@ const { HttpError, setCookies } = require('../utils');
 const User = require('../model/User');
 
 const login = async (req, res) => {
-  res.send('Login');
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    throw new HttpError(
+      'Please provide email and password',
+      StatusCodes.BAD_REQUEST
+    );
+
+  const user = await User.findOne({ email });
+
+  if (!user)
+    throw new HttpError('Invalid credentials', StatusCodes.UNAUTHORIZED);
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect)
+    throw new HttpError('Invalid credentials', StatusCodes.UNAUTHORIZED);
+
+  const tokenProps = { name: user.name, id: user._id, role: user.role };
+
+  setCookies(res, tokenProps);
+
+  const response = {
+    ...tokenProps,
+    email: user._doc.email,
+  };
+
+  res.status(StatusCodes.OK).json(response);
 };
 
 const register = async (req, res) => {
@@ -33,7 +60,12 @@ const register = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.send('logged out');
+  res.cookie('token', '', {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+
+  res.sendStatus(StatusCodes.OK);
 };
 
 module.exports = {
