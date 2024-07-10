a37baa5fb2695851f29a50ca8616b6454b4f4b8c
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-07-10 15:09:04 +0200

    add get all and single user and clean auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,24 @@
+const { StatusCodes } = require('http-status-codes');
+const User = require('../model/User');
+const { HttpError } = require('../utils');
+
 const getAllUsers = async (req, res) => {
-  res.send('get all users');
+  //other options: query.select(name email role) / Schema: password:{select: false}
+  const users = await User.find({ role: 'user' }).select('-password');
+  if (!users.length)
+    throw new HttpError('Users not found', StatusCodes.NOT_FOUND);
+
+  res.status(StatusCodes.OK).json(users);
 };
 
 const getUser = async (req, res) => {
-  res.send('get user');
+  const { id } = req.params;
+
+  const user = await User.findById(id).select('-password');
+
+  if (!user) throw new HttpError('User not found', StatusCodes.NOT_FOUND);
+
+  res.status(StatusCodes.OK).json(user);
 };
 
 const showUser = async (req, res) => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,22 +2,6 @@ const jwt = require('jsonwebtoken');
 const { StatusCodes } = require('http-status-codes');
 const { HttpError } = require('../utils');
 
-const authenticationMiddleware = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new HttpError('Invalid credentials', StatusCodes.UNAUTHORIZED);
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const testUser = decoded.id === '665f2569e18c8e87335cf4f8';
-    req.user = { ...decoded, testUser };
-    next();
-  } catch {
-    throw new HttpError('Invalid credentials', StatusCodes.UNAUTHORIZED);
-  }
-};
+const authenticationMiddleware = async (req, res, next) => {};
 
 module.exports = authenticationMiddleware;
