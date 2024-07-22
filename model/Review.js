92204d094f091fc1101ca3ca80481c2ea59363d5
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-07-22 16:58:26 +0200

    add review model

---
+++ model/Review.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    tile: { type: String, trim: true, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+ReviewSchema.index({ product: 1, user: 1 }, { unique: true });
+module.exports = mongoose.model('Review', ReviewSchema);
